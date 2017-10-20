6a0639fbc1d1fe62f2fa6d8332820ca60e1e7998
Kayla Hansen <shiverr@gmail.com>
2017-10-20 03:32:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,12 @@
 class Song
-  attr_accessor :artist
+  attr_accessor :artist, :name
 
-  def initialize
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    self.artist.name
   end
 
 end
